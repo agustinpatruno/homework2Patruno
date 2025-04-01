b4798d8c67a80cc77fe7ef37f2de3dabbe851566
agustinpatruno <agustinpatruno@gmail.com>
2025-04-01 12:55:57 -0300

    avance en el ejercicio 4.

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,4 +1,106 @@
 #include <ejercicio4.hpp>
 
+////// implementacion de los metodos de la clase cuenta_de_banco //////
 
+double cuenta_de_banco::obtenerbalance() const
+{
+    return balance;
+}
 
+void cuenta_de_banco::depositar(double monto_depositar)
+{
+    balance += monto_depositar;
+    return;
+}
+
+// constructor de la cuenta de banco //
+
+cuenta_de_banco::cuenta_de_banco(double balance, string nombretitular) : balance(balance), titularcuenta(nombretitular){}
+
+// implementacion de los metodos de caja_de_ahorro //
+
+void caja_de_ahorro::descuento()
+{
+    if (cant_veces_info > 2)
+    {
+        if (obtenerbalance() >= 20)
+        {
+            balance -= 20;
+            return;
+        }
+        cout << "no hay suficiente dinero para aplicar el descuento" << endl;
+    }
+}
+
+////// constructor de la caja de ahorro //////
+
+caja_de_ahorro::caja_de_ahorro(double balance, string nombretitular) : cuenta_de_banco(balance,nombretitular), cant_veces_info(0){}
+
+// definicion de las funciones 
+
+void caja_de_ahorro::retirar(double monto_retirar)
+{
+    if (monto_retirar < 0)
+    { 
+        throw invalid_argument("arror, no se puede retirar un monto negativo");
+        return;
+    }
+    if (monto_retirar > obtenerbalance())
+    {
+        cout << "arror, no se puede retirar un monto mayor al que hay en la cuenta de ahorro." << endl;
+        return;
+    }
+
+    balance -= monto_retirar;
+
+}
+
+void caja_de_ahorro::mostrarinfo()
+{
+    cout << "tipo de cuenta: cuenta de ahorro" << endl;
+    cout << "balance: " << balance << endl;
+    cout << "nombre del titular: " << titularcuenta << endl;
+    
+    cant_veces_info += 1;
+
+    descuento();
+}
+
+////// implementacion metodos de la clase cuentacorriente //////
+
+// constructor de la cuenta corriente // 
+
+cuentacorriente::cuentacorriente(double balance_cuenta_corriente, double balance_cuenta_ahorro, string nombretitular) :cuenta_de_banco(balance_cuenta_corriente,nombretitular)
+{
+    caja_ahorro = make_unique<caja_de_ahorro>(balance_cuenta_ahorro,nombretitular);
+}
+
+// definicion de las funciones
+
+void cuentacorriente::retirar(double monto_retirar)
+{
+    if (monto_retirar < 0)
+    {
+        throw invalid_argument("arror, no se puede retirar un monto negativo");
+        return;
+    }
+    else if (monto_retirar <= obtenerbalance())
+    {
+        balance -= monto_retirar;
+    }
+    else
+    {
+        double excedente = monto_retirar - obtenerbalance();
+
+        balance = 0;
+
+        caja_ahorro->retirar(excedente);
+    }
+}
+
+void cuentacorriente::mostrarinfo()
+{
+    cout << "tipo de cuenta: cuenta corriente" << endl;
+    cout << "balance: " << balance << endl;
+    cout << "nombre del titular: " << titularcuenta << endl;
+}
--- ejercicio4.hpp
+++ ejercicio4.hpp
@@ -5,65 +5,139 @@
 #include <memory>
 using namespace std;
 
-
 class cuenta_de_banco
 {     
-    public:
-        
-        
-        cuenta_de_banco(double balance, string nombretitular)
-        {
-            balance = balance;
-            titularcuenta = nombretitular;
-        }
-
     protected:
 
         string titularcuenta;
 
         double balance;
 
-        double obtenerbalance()
-        {
-            return balance;
-        }
+        double obtenerbalance() const;
 
-        void depositar(double monto_depositar)
-        {
-            balance += monto_depositar;
-            return;
-        }
+        void depositar(double monto_depositar);
 
-        void virtual retirar(double monto_retirar) const = 0;
+        void virtual retirar(double monto_retirar) = 0;
 
-        void virtual mostrarinfo() const = 0;
+        void virtual mostrarinfo() = 0;
+    
+    public:
+        
+        cuenta_de_banco(double balance, string nombretitular);
 };
-
 
 class caja_de_ahorro : public cuenta_de_banco
 {
     private:
 
-        int cant_veces_info ;
+        int cant_veces_info;
+
+        // funcion que realiza el descuento
+
+        void descuento();
+
+    public:
+
+        // constructor de la caja de ahorro //
+
+        caja_de_ahorro(double balance, string nombretitular);
+
+        // definicion de las funciones 
+
+        void virtual retirar(double monto_retirar) override;
+
+        void virtual mostrarinfo() override;
+};
+
+class cuentacorriente : public cuenta_de_banco
+{
+    private:
+        
+        unique_ptr<caja_de_ahorro> caja_ahorro;
 
     public:
         
-        caja_de_ahorro(double balance, string nombretitular) : cuenta_de_banco(balance,nombretitular), cant_veces_info(0){}
-        
+        friend class caja_de_ahorro;
 
-        void retirar(double monto_retirar) override
-        {
-            if (monto_retirar < 0)
-            { 
-                throw invalid_argument("arror, no se puede retirar un monto negativo");
-                return;
-            }
-            if (monto_retirar > obtenerbalance())
-            {
-                throw invalid_argument("arror, no se puede retirar un monto mayor al que hay en balance");
-                return;
-            }
-        }
+        // constructor de la cuenta corriente // 
+
+        cuentacorriente(double balance_cuenta_corriente, double balance_cuenta_ahorro, string nombretitular);
+        
+        // definicion de las funciones
+
+        void virtual retirar(double monto_retirar)override;
+
+        void virtual mostrarinfo()override;
 };
 
 #endif
+
+/*
+a)
+
+clase cuenta_de_banco:
+
+    private:
+        - vacio. esto es debido a que si defino los metodos virtuales puros aca, cuando aplico el polimorfismo en las otras 2 subclases,
+            estas no van a poder definir esas funciones debido a que van a tener un error. Esto es debido a que la parte private 
+            no permite el acceso a derivadas.
+    
+    public: 
+        - cuenta_de_banco(double balance, string nombretitular);
+            debido a que el constructor del objeto tiene que estar necesariamente en la parte pubblica para poder crear el objeto.
+        
+    protected:
+        - string titularcuenta;
+            nombre del titular de la cuenta.
+
+        - double balance;
+            balance del titular de la cuenta.
+
+        -  double obtenerbalance() const;
+            retorna el valor del balance. el const impide modificar cualquier atributo.
+
+        - void depositar(double monto_depositar);
+            le suma al balance el monto a depositar que se pasa por parametro.
+
+        - void virtual retirar(double monto_retirar) = 0;
+            metodo virtual puro. retira el monto ingresado por parametro. caso de ser negativo o de ser mayor que el balance, retorna un
+            mensaje.
+
+        - void virtual mostrarinfo() = 0;
+            metodo virtual puto. muestra la informacion del titular de la cuenta(nombre, tipo de cuenta, balance). 
+
+        todos estos metodos y atributos estan en protected para permitir que las clases derivadas hereden los mismo y puedan acceder
+        a sus funcionalidades.
+
+
+clase caja_de_ahorro: 
+    
+    private:
+        - int cant_veces_info;
+            contador de la cantidad de veces que se solicita ver la informacion de un titular. 
+
+        - void descuento();
+            realiza el incremento de la variable cant_veces_info. ademas despues de mostrar mas de 2 veces la informacion del titular,
+            se decuenta 20$ del balanca. caso de que no haya 20$ en el balance, retorna un mensaje.
+        
+        el atributo, al igual que el metodo esta en private para que no pueda ser modificado desde fuera de un objeto. solo puede ser 
+        modificado con un metodo. por que por ejemplo, si estaria en public, el usuario podria modificar el metodo, logrando que luego
+        al usar el atributo, le realize un descuento que no se deberia
+    
+    public: 
+
+        - caja_de_ahorro(double balance, string nombretitular);
+            constructor que inicializa en objeto con el balance y el nombre del titual que se le pase por parametro.
+
+        - void virtual retirar(double monto_retirar) override;
+            funcion virtual derivada de la clase cuenta_de_banco. caso de que el monto a retirar sea negativo o mayor al balance,
+            devuelve un mensaje. caso de que sea menor que el balance, se lo resta al balance.
+
+        - void virtual mostrarinfo() override;
+            funcion virtual derivada de la clase cuenta_de_banco. muestra la informacion del titular de la cuenta.
+            (tipo de cuenta = caja de ahorro)
+        
+        el constructor necesariamente tiene que estar en public para que se pueda crear el objeto.
+        la funcion retirar, debe estar en public para que el titular de la cuenta pueda retirar de su cuenta el dinero que desee. 
+        
+*/
